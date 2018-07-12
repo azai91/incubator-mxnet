15da83157741f9db45144b0fecc6385ab8d43ea0
Alexander Zai <alexzai@amazon.com>
2018-07-12 14:48:26 -0700

    update python test

--- tests/python/mkl/test_mkldnn.py
+++ tests/python/mkl/test_mkldnn.py
@@ -244,24 +244,13 @@ def test_batchnorm():
 @with_seed()
 def test_activation():
     def check_activation_training(stype):
-        for shape in [(2, 3), (2, 3, 2, 2)]:
-            data_tmp = np.random.normal(-0.1, 0.1, size=shape)
-            s = shape[1],
-            gamma = np.ones(s)
-            beta = np.ones(s)
-            gamma[1] = 3
-            beta[0] = 3
-
-            rolling_mean = np.random.uniform(size=s)
-            rolling_std = np.random.uniform(size=s)
-
-            data = mx.symbol.Variable('data', stype=stype)
-            in_location = [mx.nd.array(data_tmp).tostype(stype), mx.nd.array(gamma).tostype(stype),
-                           mx.nd.array(beta).tostype(stype)]
-            mean_std = [mx.nd.array(rolling_mean).tostype(stype), mx.nd.array(rolling_std).tostype(stype)]
-
-            test = mx.symbol.Activation(data, act_type='relu')
-            check_numeric_gradient(test, in_location, mean_std, numeric_eps=1e-2, rtol=0.16, atol=1e-2)
+        for act_type in ['relu', 'sigmoid', 'tanh', 'softrelu']:
+            for shape in [(2, 3), (2, 3,2), (2, 3, 2, 2)]:
+                data_tmp = np.random.normal(-0.1, 0.1, size=shape)
+                data = mx.symbol.Variable('data', stype=stype)
+                in_location = [mx.nd.array(data_tmp).tostype(stype)]
+                test = mx.symbol.Activation(data, act_type=)
+                check_numeric_gradient(test, in_location, numeric_eps=1e-2, rtol=0.16, atol=1e-2)
 
     stypes = ['row_sparse', 'default']
     for stype in stypes:
